244fe39c99441ec2ec83a536e7566add9732d762
Harry Whelchel <apprentice@dbc18.local>
2014-02-21 12:33:40 -0800

    Adds migration and basic models.
    Card, deck, round, and guess models. No validations but
    association methods are set up.

---
+++ flash_cards/db/migrate/20140221120543_initial_migration.rb
@@ -0,0 +1,28 @@
+class InitialMigration < ActiveRecord::Migration
+  def change
+    create_table :cards do |t|
+      t.string :term
+      t.text   :description
+      t.belongs_to :deck
+      t.timestamps
+    end
+
+    create_table :rounds do |t|
+      t.belongs_to :deck
+      t.integer    :number
+      t.timestamps
+    end
+
+    create_table :decks do |t|
+      t.string   :name
+      t.timestamps
+    end
+
+    create_table :guesses do |t|
+      t.belongs_to :round
+      t.belongs_to :card
+      t.boolean    :correct
+      t.timestamps
+    end
+  end
+end
