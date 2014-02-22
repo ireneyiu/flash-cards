af109329b79906e190c521fcb63064547a12cda6
Hunter Paull & Matthew Knudsen <pair+hpchess.mknudsen01@devbootcamp.com>
2014-02-21 16:30:31 -0800

    setting up routes

--- flash_cards/app/controllers/index.rb
+++ flash_cards/app/controllers/index.rb
@@ -1,6 +1,23 @@
+# get '/' do
+#   # Look in app/views/index.erb
+#   past_id = Round.last.id || 0
+#   Round.create()
+#   redirect "/rounds/#{past_id + 1}"
+# end
+
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  redirect "/cards/1"
+end
+
+get '/cards/:id' do
+  @card = Card.find_by_id(params[:id])
+  erb :"rounds/show"
+end
+
+post '/cards/:id' do
+  @card = Card.find_by_id(params[:id])
+  correct = @card.term == params[:term]
+  @
 end
 
 # Controller sends the card into page.
