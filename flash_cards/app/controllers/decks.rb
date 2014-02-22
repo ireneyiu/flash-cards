62a5d24853d531a385e16f87bc5997d8cf74150c
Hunter Paull & Matthew Knudsen <pair+hpchess.mknudsen01@devbootcamp.com>
2014-02-21 23:36:28 -0800

    Merge most recent version of master

---
+++ flash_cards/app/controllers/decks.rb
@@ -0,0 +1,28 @@
+get '/decks' do
+  @decks = Deck.all
+  erb :"decks/show"
+end
+
+get '/decks/new' do
+  #if user is logged in
+  erb :"decks/new"
+end
+
+post '/decks' do
+  Deck.create(name: params[:name])
+  redirect "/decks"
+end
+
+get '/decks/:deck_id/cards/new' do
+  @deck_id = params[:deck_id]
+  erb :"cards/new"
+end
+
+post '/decks/:deck_id/cards' do
+  card = Card.create(
+    term:         params[:term],
+    description:  params[:description],
+    deck_id:      params[:deck_id]
+  )
+  redirect "/decks/#{params[:deck_id]}/cards/new"
+end
