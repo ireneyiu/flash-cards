d9a93ff1242b6be3ac1ab827fadda0bcf4b6a0f1
Harry Whelchel <harrywhelchel@gmail.com>
2014-02-22 22:10:33 -0800

    Adds BCrypt hashing of passwords to app.
    Resolves merge conflicts that existed on earlier pull request.

--- flash_cards/app/models/user.rb
+++ flash_cards/app/models/user.rb
@@ -3,4 +3,24 @@ class User < ActiveRecord::Base
   has_many :rounds
   validates_uniqueness_of :name
   validates_presence_of :name, :user_password
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(user_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.user_password = @password
+  end
+
+  def self.create(params)
+    @user = User.new(params[:user])
+    @user.password = params[:password]
+    @user.save!
+    @user
+  end
+
+
 end
