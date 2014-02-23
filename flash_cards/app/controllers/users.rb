65a684b940a30725894703904869b8a1b886875d
Brittany Mazza <bmazza555@gmail.com>
2014-02-23 01:27:37 -0800

    Update specs for validations of emails. Adds field to add email on signup. Still need to work with showing the user an error if they entered the incorrect information when attempting to sign up.

--- flash_cards/app/controllers/users.rb
+++ flash_cards/app/controllers/users.rb
@@ -1,6 +1,5 @@
 get '/users/:user_id/rounds' do
   @rounds = User.find(params[:user_id]).rounds
-
   erb :"rounds/list"
 end
 
--- flash_cards/spec/factories/user.rb
+++ flash_cards/spec/factories/user.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name "test5@example.com"
+    name "Test User"
     user_password  "1234"
+    email "test5@example.com"
   end
 end
